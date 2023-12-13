4ec86739375b9fde26f4edbea8a7ea643b3a66db
PeiYun <ollylee8520@gmail.com>
2023-12-12 20:57:59 -0700

    email api connected doen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function App() {
   const { register, handleSubmit } = useForm();
   const onSubmit = async (data) => {
     console.log(data);
-    const url = process.env.REACT_APP_SERVER;
+    const url = process.env.REACT_APP_SERVER
+      ? process.env.REACT_APP_SERVER
+      : `http://localhost`;
     try {
-      const response = await axios.get(`${url}:8081/emails`, {
+      const response = await axios.post(`${url}:8082/emails`, {
         to: data.toEmail,
         subject: data.subject,
         body: data.body,
@@ -28,9 +31,9 @@ function App() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <h4>Email to</h4>
           <input {...register("toEmail")} />
-          <h4>Email to</h4>
+          <h4>Email Subject</h4>
           <input {...register("subject")} />
-          <h4>Email to</h4>
+          <h4>Email body</h4>
           <textarea {...register("body")} />
           <input className="button" type="submit" />
         </form>
--- server.js
+++ server.js
@@ -2,20 +2,40 @@ const express = require("express");
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 const apiKey = `${process.env.SENDGRID_API_KEY}`;
+const bodyParser = require("body-parser");
 console.log("SendGrid key ", apiKey);
 const app = express();
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/emails", (req, res) => {
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/emails", (req, res) => {
   sgMail.setApiKey(apiKey);
+  const data = req.body;
+  console.log("data!!!!!!!!", data);
   const msg = {
-    to: "peiyunhite@gmail.com",
+    to: data.to,
     from: "ollylee8520@gmail.com",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    subject: data.subject,
+    text: data.body,
+    html: `<strong>${data.body}</strong>`,
   };
   sgMail
     .send(msg)
