6266c58eff6440442c0057c39ac4b8ce4a207cae
PeiYun <ollylee8520@gmail.com>
2023-12-12 20:30:28 -0700

    set up the email backend and form in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    const url = process.env.REACT_APP_SERVER;
+    try {
+      const response = await axios.get(`${url}:8081/emails`, {
+        to: data.toEmail,
+        subject: data.subject,
+        body: data.body,
+      });
+      console.log("response", response);
+      if (response.data) {
+        const data = response.data;
+        console.log("response.data", data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h4>Email to</h4>
+          <input {...register("toEmail")} />
+          <h4>Email to</h4>
+          <input {...register("subject")} />
+          <h4>Email to</h4>
+          <textarea {...register("body")} />
+          <input className="button" type="submit" />
+        </form>
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require("express"); 
-const app = express(); 
-const port = process.env.PORT || 8082; 
+const express = require("express");
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+const apiKey = `${process.env.SENDGRID_API_KEY}`;
+console.log("SendGrid key ", apiKey);
+const app = express();
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/emails", (req, res) => {
-    
+  sgMail.setApiKey(apiKey);
+  const msg = {
+    to: "peiyunhite@gmail.com",
+    from: "ollylee8520@gmail.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => console.log("send mail success"))
+    .catch(console.log);
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
-}); 
+});
