61ea78988fb4d43cbcab7f82fb2258a7658c91ff
PeiYun <ollylee8520@gmail.com>
2023-12-12 21:32:59 -0700

    add check form eamil pattern

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = async (data) => {
     console.log(data);
     const url = process.env.REACT_APP_SERVER
@@ -30,11 +34,30 @@ function App() {
       <header className="App-header">
         <form onSubmit={handleSubmit(onSubmit)}>
           <h4>Email to</h4>
-          <input {...register("toEmail")} />
+          <input
+            {...register("toEmail", {
+              required: "Email Address is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address",
+              },
+            })}
+          />
+          {errors.toEmail && <p role="alert">{errors.toEmail.message}</p>}
           <h4>Email Subject</h4>
-          <input {...register("subject")} />
+          <input
+            {...register("subject", { required: "Email Subject is required" })}
+          />
+          {errors.subject && <p role="alert">{errors.subject.message}</p>}
+
           <h4>Email body</h4>
-          <textarea {...register("body")} />
+          <textarea
+            {...register("body", {
+              required: "Body info is required",
+            })}
+          />
+          {errors.body && <p role="alert">{errors.body.message}</p>}
+
           <input className="button" type="submit" />
         </form>
       </header>
